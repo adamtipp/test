a632a5f556628c01fdaafe80dc6afd63ba27b394
Adam <adamtipp@gmail.com>
2016-05-20 14:34:12 +0200

    Extended the model, tuned the admin...

--- tango_with_jango_project/rango/views.py
+++ tango_with_jango_project/rango/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Rango says...")
+    context_dict =  {'boldmessage':"This is a boldmessage...!!!"}
+    return render(request,'rango/index.html',context=context_dict)
+    #return HttpResponse("Rango says... INDEX </BR> <a href='/rango/about'>-->About</a>")
+
+
+def about(request):
+    context_dict = {'boldmessage': "This tutorial has been put togeter by Rambo, who soon will be rocking in Django, let's put it like that!"}
+    return render(request, 'rango/about.html', context=context_dict)
+    #return HttpResponse('Rango says... ABOUT </BR><a href="/rango/"">-->Index</a>')
