9dd190b1beb8499457388e7d9688c27dea06cae3
Adam <adamtipp@gmail.com>
2016-05-22 23:28:30 +0200

    category view was added

--- tango_with_jango_project/rango/models.py
+++ tango_with_jango_project/rango/models.py
@@ -2,14 +2,22 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.template.defaultfilters import slugify
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(blank=True, unique=True)
 
     class Meta:
         verbose_name_plural = 'categories'
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args,**kwargs)
+
 
     def __str__(self):
         return self.name
